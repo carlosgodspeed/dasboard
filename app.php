e1e6684d35d24e1efbec763f2d1a3d6d28b3efb0
carlosgodspeed <carlosminibics@gmail.com>
2023-07-26 15:34:28 -0300

    concluido os campos numero de vendas e total de vendas

--- app.php
+++ app.php
@@ -42,7 +42,6 @@
                     "$this->pass"
                 );
 
-                //
                 $conexao->exec('set charset utf8');
 
                 return $conexao;
@@ -104,14 +103,20 @@
 
     $conexao = new Conexao();
 
-    $dashboard->__set('data_inicio', '2018-10-01');
-    $dashboard->__set('data_fim', '2018-10-31');
+    $competencia = explode('-', $_GET['competencia']);
+    $ano = $competencia[0];
+    $mes = $competencia[1];
+
+    $dias_do_mes = cal_days_in_month(CAL_GREGORIAN, $mes, $ano);
+
+    $dashboard->__set('data_inicio', $ano.'-'.$mes.'-01');
+    $dashboard->__set('data_fim', $ano.'-'.$mes.'-'.$dias_do_mes);
 
     $bd = new bd($conexao, $dashboard);
 
     $dashboard->__set('numeroVendas', $bd->getNumeroVendas());
     $dashboard->__set('totalVendas', $bd->getTotalVendas());
-    print_r($dashboard);
+    echo json_encode($dashboard);
 
 
 ?>
--- script.js
+++ script.js
@@ -1,18 +1,39 @@
 $(document).ready(() => {
 	
     $('#documentacao').on('click',() => {
-        //$('#pagina').load('documentacao.html')
 
-        $.get('documentacao.html', data => {
+        $.post('documentacao.html', data => {
             $('#pagina').html(data)
         })
     })
 
     $('#suporte').on('click',() => {
-        //$('#pagina').load('suporte.html')
 
-        $.get('suporte.html', data => {
+        $.post('suporte.html', data => {
             $('#pagina').html(data)
         })
     })
+
+    //ajax
+    $('#competencia').on('change', e => {
+
+        let competencia = $(e.target).val()
+        console.log(competencia)
+        
+        $.ajax({
+            type: 'GET',    
+            url: 'app.php',
+            data: `competencia=${competencia}`,
+            dataType: 'json',
+            success: dados => {
+                $('#numeroVendas').html(dados.numeroVendas)
+                $('#totalVendas').html(dados.totalVendas)
+            },
+            error: erro => { console.log(erro) }
+        })
+    })
+
+
+
+
 })
